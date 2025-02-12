64efef40277e375201fd28b9389e0d8ba52a6bda
Your Name <you@example.com>
2025-02-12 15:28:04 -0500

    some notes

--- gosoc/client.go
+++ gosoc/client.go
@@ -13,6 +13,24 @@ func build_handshake() (*http.Request, error) {
 	req.Header.Add("Upgrade", "websocket")
 	req.Header.Add("Connection", "Upgrade")
 	// What's up with the key???
+	/**
+	   To prove that the handshake was received, the server has to take two
+	   pieces of information and combine them to form a response.  The first
+	   piece of information comes from the |Sec-WebSocket-Key| header field
+	   in the client handshake:
+
+	        Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==
+
+	   For this header field, the server has to take the value (as present
+	   in the header field, e.g., the base64-encoded [RFC4648] version minus
+	   any leading and trailing whitespace) and concatenate this with the
+	   Globally Unique Identifier (GUID, [RFC4122]) "258EAFA5-E914-47DA-
+	   95CA-C5AB0DC85B11" in string form, which is unlikely to be used by
+	   network endpoints that do not understand the WebSocket Protocol.  A
+	   SHA-1 hash (160 bits) [FIPS.180-3], base64-encoded (see Section 4 of
+	   [RFC4648]), of this concatenation is then returned in the server's
+	   handshake.
+		**/
 	req.Header.Add("Sec-WebSocket-Key", "dGhlIHNhbXBsZSBub25jZQ==")
 	// What are these modes :0
 	req.Header.Add("Sec-WebSocket-Protocol", "chat, superchat")
